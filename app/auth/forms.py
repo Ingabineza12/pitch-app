6c6f45336656db9714622b8109e916763c709b9a
Deborah M Ingabire <ingabineza@gmail.com>
2020-12-02 14:57:28 +0200

    modified auth folder in app folder

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,25 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import ValidationError, StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms import StringField, PasswordField,BooleanField, SubmitField
+from wtforms.validators import Required, Email, EqualTo
 from ..models import User
+from wtforms import ValidationError
 
-class LoginForm(FlaskForm):
-    username = StringField('Username',validators=[Required()])
-    password = PasswordField('Password',validators=[Required()])
-    remember = BooleanField('Remember Me!')
-    submit = SubmitField('Login')
 
-class RegForm(FlaskForm):
-    email = StringField('Your Email Address', validators=[Required(),Email()])
-    username = StringField('Enter Your Username', validators=[Required()])
-    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username', validators=[Required()])
+    password = PasswordField('password', validators=[Required(), EqualTo('password_confirm', message='Passwords must match')])
+    password_confirm=PasswordField('Confirm passwords', validators=[Required()])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self,data_field):
-        if User.query.filter_by(email = data_field.data).first():
-            raise ValidationError("The Email has already been taken!")
 
-    def validate_username(self, data_field):
+    def validate_email(self, data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError("The username has already been taken")
+            raise ValidationError('There is an account with that email')
+
+
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,34 +1,49 @@
-from flask import render_template,url_for,flash,redirect,request
+from flask import render_template, redirect, url_for, flash, request
 from . import auth
-from flask_login import login_user,login_required,logout_user
-from .forms import RegForm,LoginForm
+from flask_login import login_user, logout_user, login_required
 from ..models import User
+from .forms import LoginForm, RegistrationForm
 from .. import db
 from ..email import mail_message
 
-@auth.route('/login', methods = ['GET','POST'])
-def login():
-    form = LoginForm()
+
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first()
-        if user != None and user.verify_password(form.password.data):
-            login_user(user,form.remember.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to PitchApp", "email/welcome_user", "user.email", user = user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/login', methods = ["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        print(user)
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or Password')
-    return render_template('auth/login.html', loginform = form)
+
+
+        flash ('Invalid username or Password')
+
+
+    title = "PitchApp Login"
+    return render_template('auth/login.html', login_form = login_form, title = title)
+
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
-
-@auth.route('/signup', methods = ["GET","POST"])
-def signup():
-    form = RegForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
-        user.save_u()
-        mail_message("Welcome to Pitch-World","email/welcome_user",user.email,user=user)
-        return redirect(url_for('auth.login'))
-    return render_template('auth/signup.html', r_form = form)
+    
