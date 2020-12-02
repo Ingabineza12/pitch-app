13352b90e3a2cd94fdd3c145b421cd1f106bb9c4
Deborah M Ingabire <ingabineza@gmail.com>
2020-12-02 13:58:16 +0200

    created app

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://deborah:1224@localhost/pitches'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
