b8d22e7860c7a743d4efd2ed60be2da923f637a1
Deborah M Ingabire <ingabineza@gmail.com>
2020-12-03 17:31:03 +0200

    changes made on role class

--- app/models.py
+++ app/models.py
@@ -49,15 +49,7 @@ class User(db.Model, UserMixin):
         return f'User{self.username}'
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User', backref = 'role', lazy = "dynamic")
-
-    def __repr__(self):
-        return f'User {self.name}'
 
 class Pitch(db.Model):
         __tablename__ = 'pitches'
