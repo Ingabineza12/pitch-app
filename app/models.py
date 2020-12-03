b0e5ac3c203601448276eb58b6223416f50dad2a
Deborah M Ingabire <ingabineza@gmail.com>
2020-12-03 17:32:24 +0200

    modified models.py

--- app/models.py
+++ app/models.py
@@ -48,7 +48,15 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f'User{self.username}'
 
+class Role(db.Model):
+    __tablename__ = 'roles'
 
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy = "dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 
 class Pitch(db.Model):
